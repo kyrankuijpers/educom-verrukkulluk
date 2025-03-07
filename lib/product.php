53f03358d0c74dd25e1a3514eb85888f85bb3350
Kyran Kuijpers <kyrankuijpers@gmail.com>
2025-03-07 16:02:09 +0100

    #5 product (article) class

--- lib/product.php
+++ lib/product.php
@@ -5,21 +5,33 @@ class Product {
     // Private so it doesn't get overriden by accident.
     private $connection;
 
-    public function __construct($connection) {
+    public $product_id;
+    public $name;
+    public $descr;
+    public $price;
+    public $unit;
+    public $content;
+    public $img;
+
+    public function __construct($connection, $product_id = null, $name = '', $descr = '', $price = 0, $unit = '', $content = 0.0, $img = '') {
         $this->connection = $connection;
+        $this->product_id = $product_id;
+        $this->name = $name;
+        $this->descr = $descr;
+        $this->price = $price;
+        $this->unit = $unit;
+        $this->content = $content;
+        $this->img = $img;
     }
   
-    public function selectProduct($product_id) {
-
+    public function selectProductByProductId($product_id) {
         $sql = "SELECT * FROM product WHERE id = $product_id";
         
         $result = mysqli_query($this->connection, $sql);
         $product = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
         return($product);
-
     }
-
-
 }
+
 ?>
