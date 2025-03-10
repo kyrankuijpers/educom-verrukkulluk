c4117cf5c1e5c6ee927c7fae811c7e9e5a14a987
Kyran Kuijpers <kyrankuijpers@gmail.com>
2025-03-10 16:33:16 +0100

    #8 #9 aanpassingen RecipeInfo en Ingredient

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ $db = new Database();
 $product = new Product($db->getConnection());
 $user = new User($db->getConnection());
 $cuisine_type = new CuisineType($db->getConnection());
-$ingredient = new Ingredient($db->getConnection());
-$recipe_info = new RecipeInfo($db->getConnection());
+$ingredient = new Ingredient($db->getConnection(), $product);
+$recipe_info = new RecipeInfo($db->getConnection(), $user);
 
 /// VERWERK 
 $recipe_id = 3;
-$user_id = 1;
+$user_id = 4;
 
-$data = $recipe_info->selectRecipeInfo($recipe_id, 'R');
+$data = $recipe_info->selectRecipeInfo($recipe_id, "R");
 
 /// RETURN
 
