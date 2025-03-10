f79955473f9edaeca51f68afb627060c72d84ca6
Kyran Kuijpers <kyrankuijpers@gmail.com>
2025-03-10 09:10:49 +0100

    Product (artikel) class #5 weghalen attributes

--- lib/product.php
+++ lib/product.php
@@ -4,23 +4,7 @@ class Product {
 
     private $connection;
 
-    public $product_id;
-    public $name;
-    public $descr;
-    public $price;
-    public $unit;
-    public $content;
-    public $img;
-
-    public function __construct($connection, $product_id = null, $name = '', $descr = '', $price = 0, $unit = '', $content = 0.0, $img = '') {
-        $this->connection = $connection;
-        $this->product_id = $product_id;
-        $this->name = $name;
-        $this->descr = $descr;
-        $this->price = $price;
-        $this->unit = $unit;
-        $this->content = $content;
-        $this->img = $img;
+    public function __construct($connection) {
     }
   
     public function selectProductByProductId($product_id) {
